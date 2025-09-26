e0eafff7b3474c1d60db697fee800bb3f9e833e8
kamiya3141 <kamiyatuwu@gmail.com>
2025-09-27 01:40:33 +0900

    かなり過激な（？）ワードチョイスです。

---
+++ ghc.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TAPE_SIZE 30000
+#define MAX_BUFFER_SIZE (1024 * 2)
+
+int check_extension(const char *filename) {
+	const char *ext = strrchr(filename, '.');
+	if (!ext) return 0;
+	return strcmp(ext, ".hcrz") == 0;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s <source_file.hcrz>\n", argv[0]);
+		return 1;
+	}
+
+	if (!check_extension(argv[1])) {
+		fprintf(stderr, "Error: Source file must have .hcrz extension.\n");
+		return 1;
+	}
+
+	FILE *src = fopen(argv[1], "r");
+	if (!src) {
+		perror("Error opening source file");
+		return 1;
+	}
+
+	unsigned char tape[TAPE_SIZE] = {0};
+	unsigned char *ptr = tape;
+
+	const char *keywords_array[] = {
+		"イヤンッ♡",
+		"アハンッ♡",
+		"アン///",
+		"オ゙ッ///",
+		"イクッ~♡"
+	};
+
+	char buffer[MAX_BUFFER_SIZE];
+	while (fgets(buffer, sizeof(buffer), src)) {
+		char *p = buffer;
+
+		char *comment_pos = strstr(p, "#");
+		if (comment_pos) {
+			*comment_pos = '\0';
+		}
+
+		while (*p) {
+			if (strncmp(p, keywords_array[0], strlen(keywords_array[0])) == 0) {
+				++ptr;
+				p += strlen(keywords_array[0]);
+			} else if (strncmp(p, keywords_array[1], strlen(keywords_array[1])) == 0) {
+				--ptr;
+				p += strlen(keywords_array[1]);
+			} else if (strncmp(p, keywords_array[2], strlen(keywords_array[2])) == 0) {
+				++(*ptr);
+				p += strlen(keywords_array[2]);
+			} else if (strncmp(p, keywords_array[3], strlen(keywords_array[3])) == 0) {
+				--(*ptr);
+				p += strlen(keywords_array[3]);
+			} else if (strncmp(p, keywords_array[4], strlen(keywords_array[4])) == 0) {
+				putchar(*ptr);
+				p += strlen(keywords_array[4]);
+			} else {
+				// 無視
+				p++;
+			}
+		}
+	}
+
+	fclose(src);
+	return 0;
+}
